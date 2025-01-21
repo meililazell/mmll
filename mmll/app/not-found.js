d78d631782e51ae2d76090aeca43c0b4a51a5af9
meililazell <meililazell@gmail.com>
2025-01-21 10:03:16 -0800

    fix button error

---
+++ mmll/app/not-found.js
@@ -0,0 +1,9 @@
+// app/not-found.js
+export default function NotFound() {
+    return (
+      <div>
+        <h2>404: This page could not be found</h2>
+        <p>Please check the URL or go back home.</p>
+      </div>
+    );
+  }
