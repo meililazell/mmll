5903199419a12f02c188fab859370f579668e9a1
meililazell <meililazell@gmail.com>
2025-01-21 22:54:01 -0800

    add open graph

--- mmll/app/layout.tsx
+++ mmll/app/layout.tsx
@@ -15,7 +15,25 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Michael and Mei Li Lazell Foundation",
-  description: "To improve the lives of people in various ways",
+  description:
+    "Improving lives through targeted programs, community engagement, and sustainable initiatives that create lasting positive change.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://lazelllifoundation.org",
+    siteName: "Michael and Mei Li Lazell Foundation",
+    title: "Michael and Mei Li Lazell Foundation",
+    description:
+      "Improving lives through targeted programs, community engagement, and sustainable initiatives that create lasting positive change.",
+    images: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/MMLLFdn%20medium.jpg-tjbn936DVX3YcJCF1fWRt0Ep8xctQG.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Michael and Mei Li Lazell Foundation Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
